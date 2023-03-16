619e59913c7aa0809daef74f37b80b1f9408a867
ZgeraldZ <80286126+ZgeraldZ@users.noreply.github.com>
2023-03-16 12:15:19 +0300

    little fixes

--- bin/exercises/all_numbers/exercise3.dart
+++ bin/exercises/all_numbers/exercise3.dart
@@ -4,8 +4,8 @@ void exercise3() {
   print('This is LLT decomposition method');
   LLTDecomposition systemSecond = LLTDecomposition.fromInputData(system2);
   List<double> solution = systemSecond.solution();
-  print('Solution of the first system: $solution');
+  print('Solution of the second system: $solution');
   double dicrepancy = systemSecond.dicrepancy(solution);
-  print('Dicrepancy of the first system: $dicrepancy');
+  print('Dicrepancy of the second system: $dicrepancy');
   print('\n\n');
 }
